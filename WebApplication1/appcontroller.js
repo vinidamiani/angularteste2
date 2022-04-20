a5c9c79da2795d63d39deb82f12c8d4228be178e
vinidamiani <48492326+vinidamiani@users.noreply.github.com>
2022-04-20 12:49:21 -0300

    Criado

---
+++ WebApplication1/appcontroller.js
@@ -0,0 +1,22 @@
+﻿/// <reference path="scripts/angular.min.js" />
+
+
+var myApp = angular.module("myModule", []);
+
+myApp.controller("myController", function ($scope) {
+    var carros = [
+        { marca: "Fiat", modelo: "Uno", anoModelo: "2008" },
+        { marca: "BMW", modelo: "328i", anoModelo: "1998" },
+        { marca: "Hyundai", modelo: "Tucson", anoModelo: "2013" }
+        ];
+
+    $scope.carros = carros;
+
+    $scope.adicionarCarro = function () {
+        $scope.carros.push(carro);
+        delete $scope.carro;
+    };
+    $scope.apagarCarros = function () {
+        console.log($scope.carros);
+    };
+});
